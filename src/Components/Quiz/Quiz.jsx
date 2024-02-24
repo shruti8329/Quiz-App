66635a6a31d9f4e637c98381444a0758803b824d
shruti8329 <shruti83295435@gmail.com>
2024-02-24 17:14:57 +0530

    work in progress

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Quiz.css";
+import { data } from "../../assets/data";
 const Quiz = () => {
+  let [index, setIndex] = useState(4);
+  let [question, setQuestion] = useState(data[index]);
   return (
     <div className="container">
       <h1>Quiz App</h1>
-
       <hr />
-      <h2>Which device is required for the Internet connection?</h2>
+      <h2>
+        {index + 1}. {question.question}
+      </h2>
       <ul>
-        <li>Moderm</li>
-        <li>Router</li>
-        <li>LAN Cable</li>
-        <li>Pen Drive</li>
-        <button>Next</button>
-        <div className="index">1 of 5 Questions</div>
+        <li>{question.option1}</li>
+        <li>{question.option2}</li>
+        <li>{question.option3}</li>
+        <li>{question.option4}</li>
       </ul>
+      <button>Next</button>
+      <div className="index">1 of 5 Questions</div>
     </div>
   );
 };
