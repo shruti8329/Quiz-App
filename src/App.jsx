ee9f780538f8a12efc5494365843eb503a7a7e66
shruti8329 <shruti83295435@gmail.com>
2024-02-24 16:23:46 +0530

    work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
-import Quiz from "./components/quiz/quiz";
-
+import Quiz from "./components/Quiz/Quiz";
 const App = () => {
   return (
-    <>
+    <div>
       <Quiz />
-    </>
+    </div>
   );
 };
 
--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -4,8 +4,18 @@ const Quiz = () => {
   return (
     <div className="container">
       <h1>Quiz App</h1>
+
+      <hr />
+      <h2>Which device is required for the Internet connection?</h2>
+      <ul>
+        <li>Moderm</li>
+        <li>Router</li>
+        <li>LAN Cable</li>
+        <li>Pen Drive</li>
+        <button>Next</button>
+        <div className="index">1 of 5 Questions</div>
+      </ul>
     </div>
   );
 };
-
 export default Quiz;
